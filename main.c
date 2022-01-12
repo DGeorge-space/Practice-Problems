bc6f2395c3f5939a7f5df1f65a49d3680ad85f02
s2242111 <d.j.george@sms.ed.ac.uk>
2022-01-12 08:51:06 +0000

    Rather than using a linked list to store all of the fibonacci numbers, realised the problem could be easily solved by checking each Fibonacci number directly. The problem is now solved

--- main.c
+++ main.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
-#include "pE_problem2.c"
+
 
 void pE_problem_1();
 
 void main()
 {
-	struct node *ptr=NULL;
 
-	for(int i=0; i=10; i++)
-	{
-		add_to_list(i,TRUE);
-	}
+}
 
 
-	print_list();
-
-}	
 pE_problem_1()
 {
 
@@ -36,8 +29,45 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 			count +=i;
 		}
 	}
-	printf("count: %d \n", count);
+//	printf("count: %d \n", count);
 
+
+}
+
+pE_problem2()
+{
+/*By Considering terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even valued terms */
+
+		
+	long maxFibonacciNumber = 4000000;
+	long sumOfEvenFibNums = 2;
+
+	long fibNum_i = 1;
+	long fibNum_j = 2;
+	long fibNum_k = 0;
+	
+	while(1)
+	{
+		fibNum_k = fibNum_i + fibNum_j;
+
+		if(fibNum_k >maxFibonacciNumber){
+			break;
+		}
+
+		if(fibNum_k % 2 ==0)
+		{
+			sumOfEvenFibNums += fibNum_k;
+		}
+
+		printf("%d \n", fibNum_k);
+
+
+		fibNum_i = fibNum_j;
+		fibNum_j = fibNum_k;
+
+	}
+
+	printf("Sum of even Fibonacci Numbers up to %ld is: %ld \n", maxFibonacciNumber, sumOfEvenFibNums); 
 
 }
 
