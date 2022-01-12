fa13633a45afa1f34083b75f983e2c016f64a5d0
s2242111 <d.j.george@sms.ed.ac.uk>
2022-01-12 09:45:23 +0000

    Problem 3 plan

--- main.c
+++ main.c
@@ -5,7 +5,32 @@ void pE_problem_1();
 
 void main()
 {
+ /*Project Euler Problem 3: 
+  *
+  * Find the largest prime factor of a number, N: 
+  *
+	1:  create a list of prime numbers starting with just 2.
 
+	2:  check whether N is divisible by each number in the list of primes:
+		If it is set N = Nold / prime and do the same again recursively. 
+			Check when the new N is, itself, prime:
+				
+				if prime:
+					append to prime array 
+			       		return the prime array. note: might be better to just return value  
+
+		If not, test whether prime +1 is prime
+			if prime, append to prime list 
+
+			go back to step 2
+		       	
+		Else, test prime +1+1	
+
+	3:  check for the largest value in the prime array	
+
+  */
+
+	
 }
 
 
